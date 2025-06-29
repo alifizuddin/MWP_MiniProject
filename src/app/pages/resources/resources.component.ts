21e0d2fbbbf780931db98e3384d4a19c11bb6850
Alif Izuddin <muhammadalifizuddin@graduate.utm.my>
2025-06-29 22:08:31 +0800

    1

--- src/app/pages/resources/resources.component.ts
+++ src/app/pages/resources/resources.component.ts
@@ -1,13 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
-
+import { FoodbankService } from '../../services/foodbank.service';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-resources',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, CommonModule],
   templateUrl: './resources.component.html',
   styleUrl: './resources.component.css'
 })
-export class ResourcesComponent {
+export class ResourcesComponent implements OnInit {
+  foodbanks: any[] = [];
+
+  constructor(private foodbankService: FoodbankService) {}
+
+  ngOnInit(): void {
+    this.foodbankService.getFoodBanks().subscribe({
+      next: (data) => {
+        console.log('Fetched foodbank data:', data);
+        this.foodbanks = data;
+      },
+      error: (err) => {
+        console.error('Error fetching foodbanks:', err);
+      }
+    });
+  }
 
 }
--- src/app/services/foodbank.service.ts
+++ src/app/services/foodbank.service.ts
@@ -1,9 +1,24 @@
+// src/app/services/foodbank.service.ts
+
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, map } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FoodbankService {
+  private apiUrl = 'https://nominatim.openstreetmap.org/search?format=json&q=food+bank';
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  getFoodBanks(): Observable<any[]> {
+    return this.http.get<any[]>(this.apiUrl).pipe(
+      map(results => results.map(item => ({
+        name: item.display_name,
+        lat: item.lat,
+        lon: item.lon
+      })))
+    );
+  }
 }
